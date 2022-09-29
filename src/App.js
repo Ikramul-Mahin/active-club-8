80e91df6fbcd1cc97914f3b5e1e0fc6921e7fbf6
Ikramul-Mahin <ikramul.mahin.islam@gmail.com>
2022-09-30 06:42:00 +0800

    info updted

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Ques from './component/Ques/Ques';
 function App() {
   return (
     <div className="App">
-      <h3 className='bg-primary text-white rounded px-4 mx-4'>My Programming Routine</h3>
+      <h3 className='bg-primary text-white rounded px-4 mx-4'>My Exercise Shedule</h3>
       <h4 className='bg-primary text-white rounded px-2 mx-4'>Select The Shedule</h4>
       <Products></Products>
       <Ques></Ques>
--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -2,17 +2,20 @@ import React from 'react';
 
 const Cart = (props) => {
     const { cart } = props
+    let time = 0;
+    for (const product of cart) {
+        time = time + product.time
+    }
     return (
         <div className='border border-2 py-4'>
-            <p>cart:{cart.length}</p>
-            <div>
-                <img src="" alt="" />
+            <div className='info'>
+                <img className='pic w-25 img-fluid rounded rounded-circle' src="/person.jpg" alt="" />
                 <div>
                     <h2>Abdullah Mamun</h2>
                     <p>Dhaka,Bangladesh</p>
                 </div>
             </div>
-            <div className='d-flex ms-3'>
+            <div className='d-flex ms-3 justify-content-center bg-primary rounded'>
                 <div className='bg-primary rounded text-white mx-1 px-2'>
                     <h4>Height</h4>
                     <p>6.7ft</p>
@@ -39,7 +42,7 @@ const Cart = (props) => {
                 <h3>Exercise Details</h3>
 
                 <div className='text-left'>
-                    <h4 className='bg-secondary text-white rounded'>Study Time:</h4>
+                    <h4 className='bg-secondary text-white rounded'>Study Time:{time}</h4>
                     <h4 className='bg-secondary text-white rounded'>Break Time:</h4>
 
                     <button className='btn btn-primary'>Activity Completed</button>
