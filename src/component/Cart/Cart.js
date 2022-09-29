d7f4467a1c2346d370f2e90bb97c7045f33ee89f
Ikramul-Mahin <ikramul.mahin.islam@gmail.com>
2022-09-30 06:57:20 +0800

    layouts done

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -32,17 +32,17 @@ const Cart = (props) => {
             <div className='mt-3'>
                 <h3>Add a break</h3>
                 <div>
-                    <button className='btn-primary border-none mx-1'>20s</button>
-                    <button className='btn-primary border-none mx-1'>30s</button>
-                    <button className='btn-primary border-none mx-1'>30s</button>
-                    <button className='btn-primary border-none mx-1'>50s</button>
+                    <button className='rounded btn-primary border border-none mx-1 p-2'>20</button>
+                    <button className='rounded btn-primary border border-none mx-1 p-2'>30</button>
+                    <button className='rounded btn-primary border border-none mx-1 p-2'>30</button>
+                    <button className='rounded btn-primary border border-none mx-1 p-2'>50</button>
                 </div>
             </div>
             <div className='mt-5'>
                 <h3>Exercise Details</h3>
 
                 <div className='text-left'>
-                    <h4 className='bg-secondary text-white rounded'>Study Time:{time}</h4>
+                    <h4 className='bg-secondary text-white rounded'>Exercise Time:{time}</h4>
                     <h4 className='bg-secondary text-white rounded'>Break Time:</h4>
 
                     <button className='btn btn-primary'>Activity Completed</button>
