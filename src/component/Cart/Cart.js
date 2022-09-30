9762171c31f3d9ed79a0c54d0172cc585a01b777
Ikramul-Mahin <ikramul.mahin.islam@gmail.com>
2022-09-30 11:04:50 +0800

    break time added

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const Cart = (props) => {
-    const { cart } = props
+    const { cart } = props;
     let time = 0;
     for (const product of cart) {
         time = time + product.time
     }
+
+
+    const [breaks, setBreaks] = useState('')
+    const getBreak = (e) => {
+        setBreaks(e.target.innerHTML)
+        console.log(e.target.innerHTML)
+
+    }
+    // useEffect(() => {
+    //     fetch('/break.json')
+    //         .then(res => res.json())
+    //         .then(data => setBreaks(data))
+    // }, [])
+
+    // const [break, setBreak]= useState([])
+
+
     return (
         <div className='border border-2 py-4'>
+            <h4>break: {breaks.length}</h4>
             <div className='info'>
                 <img className='pic w-25 img-fluid rounded rounded-circle' src="/person.jpg" alt="" />
                 <div>
@@ -32,25 +51,26 @@ const Cart = (props) => {
             <div className='mt-3'>
                 <h3>Add a break</h3>
                 <div>
-                    <button className='rounded btn-primary border border-none mx-1 p-2'>20</button>
-                    <button className='rounded btn-primary border border-none mx-1 p-2'>30</button>
-                    <button className='rounded btn-primary border border-none mx-1 p-2'>30</button>
-                    <button className='rounded btn-primary border border-none mx-1 p-2'>50</button>
+                    <button onClick={getBreak} className='rounded btn-primary border border-none mx-1 p-2'>20</button>
+                    <button onClick={getBreak} className='rounded btn-primary border border-none mx-1 p-2'>30</button>
+                    <button onClick={getBreak} className='rounded btn-primary border border-none mx-1 p-2'>40</button>
+                    <button onClick={getBreak} className='rounded btn-primary border border-none mx-1 p-2'>50</button>
                 </div>
+
             </div>
             <div className='mt-5'>
                 <h3>Exercise Details</h3>
 
                 <div className='text-left'>
                     <h4 className='bg-secondary text-white rounded'>Exercise Time:{time}s</h4>
-                    <h4 className='bg-secondary text-white rounded'>Break Time:</h4>
+                    <h4 className='bg-secondary text-white rounded'>Break Time:{breaks}</h4>
 
                     <button className='btn btn-primary'>Activity Completed</button>
                 </div>
 
 
             </div>
-        </div>
+        </div >
     );
 };
 
--- src/component/Products/Products.js
+++ src/component/Products/Products.js
@@ -6,15 +6,23 @@ import './Products.css'
 const Products = () => {
     const [products, setProducts] = useState([])
     const [cart, setCart] = useState([])
+    // const [breaks, setBreaks] = useState([])
+
     useEffect(() => {
         fetch('/products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
+    // useEffect(() => {
+    //     fetch('/break.json')
+    //         .then(res => res.json())
+    //         .then(data => setBreaks(data))
+    // }, [])
     const addToCart = (products) => {
         const newCart = [...cart, products]
         setCart(newCart)
     }
+
 
     return (
         <div className='containers mt-3'>
@@ -30,7 +38,12 @@ const Products = () => {
                 }
             </div>
             <div className='menu-bar'>
-                <Cart cart={cart}></Cart>
+
+                <Cart
+                    cart={cart}
+
+                ></Cart>
+
             </div>
         </div>
     );
